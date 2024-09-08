d47a610508f5eaed6c60ce0680df25ed77b1e5f6
shouta0715 <Myheroes0715@gmail.com>
2024-09-08 15:22:13 +0900

    add: ripple

--- apps/events/ripples/src/main.ts
+++ apps/events/ripples/src/main.ts
@@ -2,15 +2,48 @@
 /* eslint-disable no-new */
 import p5 from "p5";
 
+type Ripple = {
+  x: number;
+  y: number;
+  radius: number;
+  alpha: number;
+};
+
+const ripples: Ripple[] = [];
+
 function sketch(pi: p5) {
   const p = pi;
 
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    p.noFill();
   };
 
   p.draw = () => {
-    p.background(220);
+    p.background(0);
+
+    ripples.forEach((ripple, index) => {
+      const r = ripple;
+      p.stroke(77, 215, 227, r.alpha);
+      p.strokeWeight(4);
+      p.ellipse(r.x, r.y, r.radius * 2);
+
+      r.radius += 5;
+      r.alpha -= 2;
+
+      if (r.alpha <= 0) {
+        ripples.splice(index, 1);
+      }
+    });
+  };
+
+  p.mousePressed = () => {
+    ripples.push({
+      x: p.mouseX,
+      y: p.mouseY,
+      radius: 1,
+      alpha: 255,
+    });
   };
 }
 
