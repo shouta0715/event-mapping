e15be2cbe75454dbc17d150969e511016d11812c
shouta0715 <Myheroes0715@gmail.com>
2024-09-08 14:57:54 +0900

    add: vite

--- apps/events/ripples/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import globalConfig from "@event-mapping/tailwind-config";
-import type { Config } from "tailwindcss";
-
-const config: Pick<Config, "presets" | "content"> = {
-  content: ["./src/**/*.tsx"],
-  presets: [globalConfig],
-};
-
-export default config;
