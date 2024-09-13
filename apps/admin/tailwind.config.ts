ff566599c587ec743f1e112be85d09ad1ddf92fe
shouta0715 <Myheroes0715@gmail.com>
2024-09-13 22:09:00 +0900

    fix: global layout

--- apps/admin/tailwind.config.ts
+++ apps/admin/tailwind.config.ts
@@ -1,12 +1,20 @@
 import globalConfig from "@event-mapping/tailwind-config";
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
-const config: Pick<Config, "presets" | "content"> = {
+const config: Pick<Config, "presets" | "content" | "theme"> = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-roboto)", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
   presets: [globalConfig],
 };
 
