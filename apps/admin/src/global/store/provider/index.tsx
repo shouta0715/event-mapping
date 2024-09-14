69308e38e086edb725804968fd1633c2b3f132f3
shouta0715 <Myheroes0715@gmail.com>
2024-09-14 11:37:54 +0900

    add: state provider

---
+++ apps/admin/src/global/store/provider/index.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { useRef } from "react";
+import { useStore } from "zustand";
+import { useShallow } from "zustand/react/shallow";
+import { createTerminalState, ReturnCreateTerminalState } from "@/global/store";
+import { FlowGlobalState, FlowState } from "@/global/store/types";
+
+type EventMappingProviderProps = {
+  children: React.ReactNode;
+  defaultState?: FlowState;
+};
+
+const Context = React.createContext<ReturnCreateTerminalState | null>(null);
+
+const fallbackState: FlowState = {
+  nodes: [],
+  edges: [],
+};
+
+export const TerminalStateProvider = ({
+  children,
+  defaultState = fallbackState,
+}: EventMappingProviderProps) => {
+  const store = useRef<ReturnCreateTerminalState | null>(null);
+
+  if (!store.current) {
+    store.current = createTerminalState(defaultState);
+  }
+
+  return <Context.Provider value={store.current}>{children}</Context.Provider>;
+};
+
+const useTerminalStateContext = () => {
+  const context = React.useContext(Context);
+
+  if (!context) throw new Error("Provider is not found");
+
+  return context;
+};
+
+export const useTerminalState = (
+  selector: (state: FlowGlobalState) => Partial<FlowGlobalState>
+) => {
+  const store = useTerminalStateContext();
+
+  return useStore(store, useShallow(selector));
+};
