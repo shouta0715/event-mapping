5a778b08f8966a42da1881f08b67843cd462ec60
shouta0715 <Myheroes0715@gmail.com>
2024-09-14 19:51:06 +0900

    add: node constant

---
+++ apps/admin/src/constant/node/index.ts
@@ -0,0 +1,5 @@
+export const TERMINAL_NODE_TYPE = "terminal" as const;
+export const TERMINAL_NODE_ID = "terminal-node" as const;
+
+export const IFRAME_NODE_TYPE = "iframe" as const;
+export const IFRAME_NODE_ID = "iframe-node" as const;
