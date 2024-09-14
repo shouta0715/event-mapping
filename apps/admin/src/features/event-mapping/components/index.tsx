f664d670048c28007f36b76b163f771c017ed1c2
shouta0715 <Myheroes0715@gmail.com>
2024-09-14 11:38:42 +0900

    add: basic store usage

--- apps/admin/src/features/event-mapping/components/index.tsx
+++ apps/admin/src/features/event-mapping/components/index.tsx
@@ -1,12 +1,31 @@
+"use client";
+
 import { Background, Controls, ReactFlow } from "@xyflow/react";
 import React from "react";
 
 import "@xyflow/react/dist/style.css";
+import { useTerminalState } from "@/global/store/provider";
 
 export function EventMapping() {
+  const { nodes, edges, onEdgesChange, onNodesChange } = useTerminalState(
+    (state) => ({
+      nodes: state.nodes,
+      edges: state.edges,
+      onEdgesChange: state.onEdgesChange,
+      onNodesChange: state.onNodesChange,
+    })
+  );
+
   return (
     <div className="h-screen">
-      <ReactFlow>
+      <ReactFlow
+        edges={edges}
+        fitView
+        minZoom={0.1}
+        nodes={nodes}
+        onEdgesChange={onEdgesChange}
+        onNodesChange={onNodesChange}
+      >
         <Background />
         <Controls />
       </ReactFlow>
