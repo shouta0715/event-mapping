bfb91a3184f51995ad1dd60431f4e7af4bbb6122
shouta0715 <Myheroes0715@gmail.com>
2024-09-15 14:53:08 +0900

    delete: files

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,15 +1,5 @@
 import { Hono } from "hono";
 
-import { app as admin } from "@/routes/admin";
-import { app as images } from "@/routes/images";
-import { app as users } from "@/routes/users";
-import { SyncEnv } from "@/types/env";
+const app = new Hono();
 
-const app = new Hono<SyncEnv>();
-
-app.route("/", admin);
-app.route("/", users);
-app.route("/", images);
-
-export { WebMultiViewSession } from "@/session";
 export default app;
