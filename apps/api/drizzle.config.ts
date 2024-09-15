e2d9707458cf89bab29bc39a6ead0f6ce2700672
shouta0715 <Myheroes0715@gmail.com>
2024-09-15 15:43:11 +0900

    add: drizzle

---
+++ apps/api/drizzle.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig, Config } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./src/db/schema.ts",
+  out: "./src/db/migrations",
+  dialect: "sqlite",
+  dbCredentials: {
+    url: "./.wrangler/state/v3/d1/miniflare-D1DatabaseObject/34d299c3ce1ac43b4463ff5104d1a6d7b80456f1c43f28146c90269b78c9d14d.sqlite",
+  },
+}) satisfies Config;
---
+++ apps/api/src/db/migrations/0000_greedy_tony_stark.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `events` (
+	`id` text PRIMARY KEY NOT NULL,
+	`name` text NOT NULL,
+	`created_at` integer DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`updated_at` integer DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `sources` (
+	`id` text PRIMARY KEY NOT NULL,
+	`event_id` text NOT NULL,
+	`url` text NOT NULL,
+	`width` integer DEFAULT 1920 NOT NULL,
+	`height` integer DEFAULT 1080 NOT NULL,
+	FOREIGN KEY (`event_id`) REFERENCES `events`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `events_name_unique` ON `events` (`name`);
