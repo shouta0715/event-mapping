a769a000ed8a00518b9b0d684c7471ae09eacebb
shouta0715 <Myheroes0715@gmail.com>
2024-09-16 10:48:01 +0900

    add: db api schema

---
+++ packages/db/src/events/index.ts
@@ -0,0 +1,14 @@
+import { createInsertSchema } from "drizzle-zod";
+import { events } from "schema";
+import * as z from "zod";
+
+const eventInsertSchema = createInsertSchema(events, {
+  name: z
+    .string({ message: "名前は1文字以上255文字以下で入力してください" })
+    .min(1, { message: "名前は1文字以上255文字以下で入力してください" })
+    .max(255, { message: "名前は1文字以上255文字以下で入力してください" }),
+}).pick({ name: true });
+
+type EventInsert = z.infer<typeof eventInsertSchema>;
+
+export { eventInsertSchema, type EventInsert };
---
+++ packages/db/src/sources/index.ts
@@ -0,0 +1,14 @@
+import { createInsertSchema } from "drizzle-zod";
+import { sources } from "schema";
+import * as z from "zod";
+
+const sourceInsertSchema = createInsertSchema(sources, {
+  url: z.string().min(1).url({ message: "URLの形式が正しくありません。" }),
+  devUrl: z.string().min(1).url({ message: "URLの形式が正しくありません。" }),
+  width: z.number().min(1, { message: "1以上の正の整数を入力してください。" }),
+  height: z.number().min(1, { message: "1以上の正の整数を入力してください。" }),
+}).pick({ url: true, devUrl: true, width: true, height: true });
+
+type SourceInsert = z.infer<typeof sourceInsertSchema>;
+
+export { sourceInsertSchema, type SourceInsert };
