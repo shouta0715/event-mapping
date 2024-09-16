544b88fbf4d25edfa79b6528680070d830978165
shouta0715 <Myheroes0715@gmail.com>
2024-09-16 20:16:53 +0900

    add: form

--- packages/ui/src/components/index.tsx
+++ packages/ui/src/components/index.tsx
@@ -11,3 +11,18 @@ export {
   DialogTitle,
   DialogDescription,
 } from "./ui/dialog";
+
+export {
+  useFormField,
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+} from "./ui/form";
+
+export { Label } from "./ui/label";
+
+export { Input } from "./ui/input";
