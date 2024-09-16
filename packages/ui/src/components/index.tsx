b09932a6f327afdcec7895fb70b2aff467395612
shouta0715 <Myheroes0715@gmail.com>
2024-09-16 20:11:59 +0900

    add: dialog

--- packages/ui/src/components/index.tsx
+++ packages/ui/src/components/index.tsx
@@ -1,1 +1,13 @@
 export { Button } from "./ui/button";
+export {
+  Dialog,
+  DialogPortal,
+  DialogOverlay,
+  DialogClose,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+} from "./ui/dialog";
