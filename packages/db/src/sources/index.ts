8ab83ca4da56755aadb73cc63b0ee257ceaf9094
shouta0715 <Myheroes0715@gmail.com>
2024-09-18 00:25:08 +0900

    fix: source input schema

--- packages/db/src/sources/index.ts
+++ packages/db/src/sources/index.ts
@@ -13,13 +13,29 @@ const sourceInsertSchema = createInsertSchema(sources, {
     .regex(/^[a-zA-Z0-9_-]+$/, {
       message: "英数字とハイフン、アンダースコアのみ使用できます。",
     }),
+  name: z.string().min(1, { message: "必須入力です。" }),
   dev_url: z
     .string()
     .min(1, { message: "必須入力です。" })
     .url({ message: "URLの形式が正しくありません。" }),
-  width: z.number().min(1, { message: "整数を入力してください。" }),
-  height: z.number().min(1, { message: "整数を入力してください。" }),
-}).pick({ url: true, dev_url: true, width: true, height: true });
+  width: z.coerce
+    .number({
+      message: "整数を入力してください。",
+    })
+    .min(1, { message: "整数を入力してください。" }),
+  height: z.coerce
+    .number({
+      message: "整数を入力してください。",
+    })
+    .min(1, { message: "整数を入力してください。" }),
+}).pick({
+  name: true,
+  url: true,
+  slug: true,
+  dev_url: true,
+  width: true,
+  height: true,
+});
 
 type SourceInsert = z.infer<typeof sourceInsertSchema>;
 type Source = typeof sources.$inferSelect;
