c87ea27d1b98c34c52a91aa778c6737e7ecdb68d
shouta0715 <Myheroes0715@gmail.com>
2024-09-22 14:00:34 +0900

    add: basic wbesocket provider

---
+++ apps/admin/src/features/websocket/components/index.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React, { useMemo } from "react";
+import { Loader } from "@/components/animation";
+import { useWs } from "@/features/websocket/hooks";
+
+type WebSocketStatus = {
+  isOpen: boolean;
+  isClosing: boolean;
+  isClosed: boolean;
+  isUninstantiated: boolean;
+  status: "connecting" | "open" | "closing" | "closed" | "uninstantiated";
+};
+
+const INITIAL_STATE: WebSocketStatus = {
+  isOpen: false,
+  isClosing: false,
+  isClosed: false,
+  isUninstantiated: false,
+  status: "uninstantiated",
+};
+
+const WSStatusContext = React.createContext<WebSocketStatus>(INITIAL_STATE);
+
+export function WSStatusProvider({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  const { status } = useWs();
+
+  const statusValue: WebSocketStatus = useMemo(
+    () => ({
+      isOpen: status === "open",
+      isClosing: status === "closing",
+      isClosed: status === "closed",
+      isUninstantiated: status === "uninstantiated",
+      status,
+    }),
+    [status]
+  );
+
+  if (status === "connecting") return <Loader />;
+
+  return (
+    <WSStatusContext.Provider value={statusValue}>
+      {children}
+    </WSStatusContext.Provider>
+  );
+}
+
+export function useWSStatus() {
+  return React.useContext(WSStatusContext);
+}
---
+++ apps/admin/src/features/websocket/hooks/index.ts
@@ -0,0 +1,57 @@
+import { useParams } from "next/navigation";
+
+import useWebSocket, { ReadyState } from "react-use-websocket";
+import { env } from "@/env";
+
+const getWsUrl = (sourceId: string) => {
+  return `${env.NEXT_PUBLIC_WS_URL}/sources/${sourceId}/subscribe/admin`;
+};
+
+export function useWs<T = unknown>() {
+  const params = useParams<{ "source-id": string }>();
+  const url = getWsUrl(params["source-id"]);
+
+  const { sendMessage, lastMessage, readyState } = useWebSocket<T>(url, {
+    reconnectInterval: (attemptNumber) =>
+      Math.min(2 ** attemptNumber * 1000, 10000),
+    reconnectAttempts: 10,
+    shouldReconnect: (e) => e.code !== 1000,
+    share: true,
+    heartbeat: {
+      message: "ping",
+      returnMessage: "pong",
+      timeout: 5 * 60 * 1000,
+      interval: 1 * 60 * 1000,
+    },
+    filter: ({ data }) => {
+      if (typeof data === "string" && data === "pong") return false;
+
+      return true;
+    },
+  });
+
+  const status = {
+    [ReadyState.CONNECTING]: "connecting" as const,
+    [ReadyState.OPEN]: "open" as const,
+    [ReadyState.CLOSING]: "closing" as const,
+    [ReadyState.CLOSED]: "closed" as const,
+    [ReadyState.UNINSTANTIATED]: "uninstantiated" as const,
+  }[readyState];
+
+  const isOpen = readyState === ReadyState.OPEN;
+  const isConnecting = readyState === ReadyState.CONNECTING;
+  const isClosing = readyState === ReadyState.CLOSING;
+  const isClosed = readyState === ReadyState.CLOSED;
+  const isUninstantiated = readyState === ReadyState.UNINSTANTIATED;
+
+  return {
+    status,
+    isOpen,
+    isConnecting,
+    isClosing,
+    isClosed,
+    isUninstantiated,
+    sendMessage,
+    lastMessage,
+  };
+}
