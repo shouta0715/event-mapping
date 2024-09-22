0f71c4049339f663b17a69d38ba49678dcdb08e9
shouta0715 <Myheroes0715@gmail.com>
2024-09-22 15:42:09 +0900

    chore: import

--- apps/admin/src/features/event-mapping/components/index.tsx
+++ apps/admin/src/features/event-mapping/components/index.tsx
@@ -1,18 +1,31 @@
 "use client";
 
-import { Background, Controls, ReactFlow } from "@xyflow/react";
-import React from "react";
-
 import "@xyflow/react/dist/style.css";
+
+import { Event, Source } from "@event-mapping/db";
+import {
+  Background,
+  Controls,
+  Panel as RfPanel,
+  ReactFlow,
+} from "@xyflow/react";
+import React from "react";
+import { Rnd } from "react-rnd";
 import { IFRAME_NODE_TYPE } from "@/constant/node";
 import { useEventMapping } from "@/features/event-mapping/hooks";
 import { IframeNode } from "@/features/iframe/components";
+import { Panel } from "@/features/panel/components";
 
 const nodeTypes = {
   [IFRAME_NODE_TYPE]: IframeNode,
 };
 
-function EventMapping() {
+type Props = {
+  event: Event;
+  source: Source;
+};
+
+function EventMapping({ event, source }: Props) {
   const {
     nodes,
     edges,
@@ -37,6 +50,39 @@ function EventMapping() {
         onNodesChange={onNodesChange}
         onPaneClick={onPanClick}
       >
+        <RfPanel
+          className="relative h-full"
+          position="top-left"
+          style={{ margin: "0" }}
+        >
+          <Rnd
+            className="rounded-2xl border-2 bg-background/80 p-4"
+            default={{
+              height: "96%",
+              width: 320,
+              x: 20,
+              y: 20,
+            }}
+            disableDragging
+            enableResizing={{
+              bottom: false,
+              bottomLeft: false,
+              bottomRight: false,
+              left: false,
+              right: true,
+              top: false,
+              topLeft: false,
+              topRight: false,
+            }}
+            maxWidth="90vw"
+            minWidth={320}
+            position={{ x: 20, y: 20 }}
+          >
+            <div>
+              <Panel event={event} source={source} />
+            </div>
+          </Rnd>
+        </RfPanel>
         <Background />
         <Controls />
       </ReactFlow>
