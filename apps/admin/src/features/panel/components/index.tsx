234deae466049bf16e22da18c5392dd5b155de26
shouta0715 <Myheroes0715@gmail.com>
2024-09-23 15:20:07 +0900

    add: copy source id

--- apps/admin/src/features/panel/components/index.tsx
+++ apps/admin/src/features/panel/components/index.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import { Event, Source } from "@event-mapping/db";
-import React from "react";
+import { Button } from "@event-mapping/ui/components/button";
+import copy from "copy-to-clipboard";
+import { Clipboard, ClipboardCheck } from "lucide-react";
+import React, { useEffect, useRef, useState } from "react";
 import { IconPanel } from "@/features/panel/components/icon";
 import { useOnline } from "@/hooks/online";
 
@@ -12,6 +15,24 @@ type Props = {
 
 export function Panel({ event, source }: Props) {
   const isOnline = useOnline();
+  const [copied, setCopied] = useState(false);
+
+  const timer = useRef<NodeJS.Timeout | null>(null);
+
+  const handleCopy = () => {
+    copy(source.id);
+    setCopied(true);
+    timer.current = setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (!timer.current) return;
+      clearTimeout(timer.current);
+    };
+  }, []);
 
   return (
     <div>
@@ -24,6 +45,39 @@ export function Panel({ event, source }: Props) {
         </div>
         <p className="mt-1 line-clamp-2 text-sm text-muted-foreground">
           {event.name}
+        </p>
+      </div>
+
+      <hr className="my-4" />
+
+      <div className="items-center text-sm text-muted-foreground ">
+        <span className="font-semibold text-black">識別子</span>
+        <p className="flex items-center">
+          <span className="font-mono">{source.id}</span>
+          <Button
+            className="relative ml-2 shrink-0 text-xs text-muted-foreground"
+            onClick={handleCopy}
+            size="icon"
+            type="button"
+            variant="ghost"
+          >
+            {copied && (
+              <span className="absolute -top-3 left-1/2 -translate-x-1/2 text-xs text-green-600">
+                Copied!
+              </span>
+            )}
+            {copied ? (
+              <>
+                <ClipboardCheck className="rotate-12 text-green-600" />
+                <span className="sr-only">{source.id}をコピーしました</span>
+              </>
+            ) : (
+              <>
+                <Clipboard />
+                <span className="sr-only">{source.id}をコピーする</span>
+              </>
+            )}
+          </Button>
         </p>
       </div>
 
