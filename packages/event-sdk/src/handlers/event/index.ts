0b1d8566c2940836998d04b10984ba2f9bd66a3d
shouta0715 <Myheroes0715@gmail.com>
2024-09-23 21:07:31 +0900

    add: basic connect websocket from event sdk

--- packages/event-sdk/src/handlers/event/index.ts
+++ packages/event-sdk/src/handlers/event/index.ts
@@ -1,9 +1,23 @@
 import p5 from "p5";
+import { BaseHandler } from "@event-mapping/event-sdk/handlers/base";
+import { getWebsocketClient } from "@event-mapping/event-sdk/handlers/event/websocket";
+import { connectWebsocket } from "@event-mapping/event-sdk/handlers/event/websocket/connect";
 import { EventClientOptions } from "@event-mapping/event-sdk/types";
 
-export class EventHandler {
-  constructor(
-    private readonly p: p5,
-    private readonly options: EventClientOptions
-  ) {}
+export class EventHandler extends BaseHandler {
+  protected readonly ws: WebSocket;
+
+  private readonly getWebSocketClient = getWebsocketClient.bind(this);
+
+  private readonly connectWebsocket = connectWebsocket.bind(this);
+
+  constructor(p: p5, options: EventClientOptions) {
+    super(p, options);
+    this.ws = this.getWebSocketClient();
+    this.init();
+  }
+
+  private init() {
+    this.connectWebsocket();
+  }
 }
