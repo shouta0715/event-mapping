257600f845f2a981bf4727d4b1e6f9b7c5db4f33
shouta0715 <Myheroes0715@gmail.com>
2024-09-24 21:11:04 +0900

    chore

--- apps/admin/src/features/websocket/hooks/index.ts
+++ apps/admin/src/features/websocket/hooks/index.ts
@@ -1,4 +1,8 @@
-import { AdminAction, parseActionMessage } from "@event-mapping/schema";
+import {
+  AdminAction,
+  AdminWarning,
+  parseActionMessage,
+} from "@event-mapping/schema";
 import useWebSocket, { ReadyState } from "react-use-websocket";
 import { toast } from "sonner";
 import { env } from "@/env";
@@ -13,38 +17,38 @@ const getWsUrl = (id: string) => {
 export function useWs(id: string) {
   const url = getWsUrl(id);
 
-  const { sendMessage, readyState, lastJsonMessage } =
-    useWebSocket<AdminAction>(url, {
-      reconnectInterval: (attemptNumber) =>
-        Math.min(2 ** attemptNumber * 1000, 10000),
-      reconnectAttempts: 10,
-      shouldReconnect: (e) => e.code !== 1000,
-      share: true,
-      heartbeat: {
-        message: "ping",
-        returnMessage: "pong",
-        timeout: 5 * 60 * 1000,
-        interval: 1 * 60 * 1000,
-      },
-      filter: ({ data }) => {
-        console.log(data);
-        if (typeof data === "string" && data === "pong") return false;
-        const d = parseActionMessage("admin", data);
+  const { sendMessage, readyState, lastJsonMessage } = useWebSocket<
+    Exclude<AdminAction, AdminWarning>
+  >(url, {
+    reconnectInterval: (attemptNumber) =>
+      Math.min(2 ** attemptNumber * 1000, 10000),
+    reconnectAttempts: 10,
+    shouldReconnect: (e) => e.code !== 1000,
+    share: true,
+    heartbeat: {
+      message: "ping",
+      returnMessage: "pong",
+      timeout: 5 * 60 * 1000,
+      interval: 1 * 60 * 1000,
+    },
+    filter: ({ data }) => {
+      if (typeof data === "string" && data === "pong") return false;
+      const d = parseActionMessage("admin", data);
 
-        if (!d) return false;
-        const { action } = d;
+      if (!d) return false;
+      const { action } = d;
 
-        if (action === "warning") {
-          // eslint-disable-next-line no-console
-          console.warn(d.message);
-          toast.warning(d.message);
+      if (action === "warning") {
+        // eslint-disable-next-line no-console
+        console.warn(d.message);
+        toast.warning(d.message);
 
-          return false;
-        }
+        return false;
+      }
 
-        return true;
-      },
-    });
+      return true;
+    },
+  });
 
   const status = {
     [ReadyState.CONNECTING]: "connecting" as const,
