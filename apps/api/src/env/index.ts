3ecd3a51e15e47cf42142408e6288fdc64dd67f2
shouta0715 <Myheroes0715@gmail.com>
2024-09-28 23:44:37 +0900

    chroe

--- apps/api/src/env/index.ts
+++ apps/api/src/env/index.ts
@@ -1,9 +1,10 @@
 import { generateDB } from "@event-mapping/db";
+import { Subscription } from "@/subscription";
 
 export type Env = {
   Bindings: {
     DB: D1Database;
-    SUBSCRIPTION: DurableObjectNamespace;
+    SUBSCRIPTION: DurableObjectNamespace<Subscription>;
   };
   Variables: {
     db: ReturnType<typeof generateDB>;
