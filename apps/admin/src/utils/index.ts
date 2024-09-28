60eae64c297ebdf60a10eb1daa673e336812a541
shouta0715 <Myheroes0715@gmail.com>
2024-09-28 23:41:25 +0900

    add: assertTerminalNode

--- apps/admin/src/utils/index.ts
+++ apps/admin/src/utils/index.ts
@@ -1,6 +1,12 @@
+import { NodeType, TerminalNode } from "@/global/store/types";
+
 export const getQ = (searchParams: { name: string | undefined | string[] }) => {
   const q =
     typeof searchParams.name === "string" ? searchParams.name : undefined;
 
   return q;
+};
+
+export const assertTerminalNode = (node: NodeType): node is TerminalNode => {
+  return node.type === "terminal";
 };
