5019e1179ee0147c26c189923a700092fe667a24
shouta0715 <Myheroes0715@gmail.com>
2024-10-11 10:49:42 +0900

    chore

--- packages/event-sdk/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ["@event-mapping/eslint-config/typescript.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-};
