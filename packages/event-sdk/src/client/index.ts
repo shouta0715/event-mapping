b690ae6a14c9e19db2724d365fd4590664b432a3
shouta0715 <Myheroes0715@gmail.com>
2024-10-13 19:31:15 +0900

    fix: types

--- packages/event-sdk/src/client/index.ts
+++ packages/event-sdk/src/client/index.ts
@@ -3,12 +3,17 @@ import { AdminHandler } from "@event-mapping/event-sdk/handlers/admin";
 import { EventHandler } from "@event-mapping/event-sdk/handlers/event";
 import { EventClientOptions } from "@event-mapping/event-sdk/types";
 
-export const createEventClient = (p: p5, options: EventClientOptions) => {
+export const createEventClient = <
+  TMeta extends Record<string, unknown> = Record<string, unknown>,
+>(
+  p: p5,
+  options: EventClientOptions
+) => {
   const isIframe = window.self !== window.top;
 
   const handler = isIframe
-    ? new AdminHandler(p, options)
-    : new EventHandler(p, options);
+    ? new AdminHandler<TMeta>(p, options)
+    : new EventHandler<TMeta>(p, options);
 
   return handler;
 };
