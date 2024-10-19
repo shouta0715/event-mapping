51f3443c351343dcb60980bfc83b9c0804033bdd
shouta0715 <Myheroes0715@gmail.com>
2024-10-19 14:54:06 +0900

    add: scale?

--- packages/event-sdk/src/handlers/helper/index.ts
+++ packages/event-sdk/src/handlers/helper/index.ts
@@ -2,11 +2,18 @@ import { EventHandler } from "@event-mapping/event-sdk/handlers/event";
 
 export function transform(this: EventHandler, cb: () => void) {
   if (!this.terminal) return;
-  const { startX, startY } = this.terminal;
+  const { startX, startY, width, height, windowHeight, windowWidth } =
+    this.terminal;
+
+  const scaleX = windowWidth / width;
+  const scaleY = windowHeight / height;
+
+  const x = -startX * scaleX;
+  const y = -startY * scaleY;
 
   this.p.push();
-  this.p.translate(-startX, -startY);
-  this.p.scale(1);
+  this.p.translate(x, y);
+  this.p.scale(scaleX, scaleY);
   cb();
   this.p.pop();
 }
