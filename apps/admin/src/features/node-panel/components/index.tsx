65ac219caa7ca08417ffed18db992e7dcadeb365
shouta0715 <Myheroes0715@gmail.com>
2024-11-04 16:01:31 +0900

    add: terminal menu

--- apps/admin/src/features/node-panel/components/index.tsx
+++ apps/admin/src/features/node-panel/components/index.tsx
@@ -4,9 +4,11 @@ import {
   AccordionTrigger,
   AccordionContent,
 } from "@event-mapping/ui/components/accordion";
+
 import { cn } from "@event-mapping/ui/lib/utils";
 import React from "react";
 import { NodeForm } from "@/features/node-form/components";
+import { TerminalSetting } from "@/features/terminal-setting/components";
 import { useNodeHandler } from "@/hooks/node";
 
 type NodePanelProps = {
@@ -21,12 +23,17 @@ export function NodePanel({ data, nodeId, isSelected }: NodePanelProps) {
   return (
     <div>
       <AccordionItem value={data.id}>
-        <AccordionTrigger
-          className={cn("px-2", isSelected && "text-primary")}
-          onClick={() => setNode(data.id)}
-        >
-          {data.displayname}
-        </AccordionTrigger>
+        <div className="flex w-full items-center">
+          <div className="flex-1">
+            <AccordionTrigger
+              className={cn("px-2 flex-1", isSelected && "text-primary")}
+              onClick={() => setNode(data.id)}
+            >
+              {data.displayname}
+            </AccordionTrigger>
+          </div>
+          <TerminalSetting data={data} nodeId={nodeId} />
+        </div>
         <AccordionContent className="px-2">
           <NodeForm
             key={JSON.stringify(data)}
