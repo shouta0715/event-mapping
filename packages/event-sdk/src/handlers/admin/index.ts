83e30445c1e93fc954817d23f3dafc2f361acca6
shouta0715 <Myheroes0715@gmail.com>
2024-11-10 19:56:11 +0900

    fix: wrap transform

--- packages/event-sdk/src/handlers/admin/index.ts
+++ packages/event-sdk/src/handlers/admin/index.ts
@@ -24,19 +24,19 @@ export class AdminHandler<
   transform = adminTransform.bind(this);
 
   circle: EventClient["circle"] = (x, y, d) => {
-    this.p.circle(x, y, d);
+    this.transform(() => this.p.circle(x, y, d));
   };
 
   ellipse: EventClient["ellipse"] = (x, y, w, h) => {
-    this.p.ellipse(x, y, w, h);
+    this.transform(() => this.p.ellipse(x, y, w, h));
   };
 
   line: EventClient["line"] = (x1, y1, x2, y2) => {
-    this.p.line(x1, y1, x2, y2);
+    this.transform(() => this.p.line(x1, y1, x2, y2));
   };
 
   point: EventClient["point"] = (x, y, z) => {
-    this.p.point(x, y, z);
+    this.transform(() => this.p.point(x, y, z));
   };
 
   quad: EventClient["quad"] = (
@@ -51,18 +51,20 @@ export class AdminHandler<
     detailX,
     detailY
   ) => {
-    this.p.quad(x1, y1, x2, y2, x3, y3, x4, y4, detailX, detailY);
+    this.transform(() =>
+      this.p.quad(x1, y1, x2, y2, x3, y3, x4, y4, detailX, detailY)
+    );
   };
 
   rect: EventClient["rect"] = (x, y, w, h, tl, tr, br, bl) => {
-    this.p.rect(x, y, w, h, tl, tr, br, bl);
+    this.transform(() => this.p.rect(x, y, w, h, tl, tr, br, bl));
   };
 
   square: EventClient["square"] = (x, y, s, tl, tr, br, bl) => {
-    this.p.square(x, y, s, tl, tr, br, bl);
+    this.transform(() => this.p.square(x, y, s, tl, tr, br, bl));
   };
 
   triangle: EventClient["triangle"] = (x1, y1, x2, y2, x3, y3) => {
-    this.p.triangle(x1, y1, x2, y2, x3, y3);
+    this.transform(() => this.p.triangle(x1, y1, x2, y2, x3, y3));
   };
 }
