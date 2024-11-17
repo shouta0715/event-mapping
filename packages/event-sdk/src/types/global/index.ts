c5b816d3e41de17f35ba051950a7b32af0aef20a
shouta0715 <Myheroes0715@gmail.com>
2024-11-17 23:28:26 +0900

    add: CanSerialize type

--- packages/event-sdk/src/types/global/index.ts
+++ packages/event-sdk/src/types/global/index.ts
@@ -3,4 +3,8 @@ export type GlobalData = {
   height: number;
 };
 
+export type CanSerialize = string | number | boolean | null | TTData | TTData[];
+
 export type TTData = Record<string, unknown>;
+
+export type ShapeMeta = TTData;
