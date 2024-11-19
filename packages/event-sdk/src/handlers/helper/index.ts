4354ac38e16194b6f0ec32cf6ee2c8de9afe6d1c
shouta0715 <Myheroes0715@gmail.com>
2024-11-19 21:22:43 +0900

    add: transformed

--- packages/event-sdk/src/handlers/helper/index.ts
+++ packages/event-sdk/src/handlers/helper/index.ts
@@ -54,3 +54,57 @@ export function adminTransform(this: AdminHandler, cb: () => void) {
   cb();
   this.p.pop();
 }
+
+type TransformedResult = {
+  x: number;
+  y: number;
+  scaleX: number;
+  scaleY: number;
+};
+
+export function transformed(
+  this: EventHandler,
+  { x, y }: { x: number; y: number }
+): TransformedResult {
+  if (!this.terminal) return { x: 0, y: 0, scaleX: 1, scaleY: 1 };
+
+  const { startX, startY, width, height, windowHeight, windowWidth } =
+    this.terminal;
+
+  const scaleX = windowWidth / width;
+  const scaleY = windowHeight / height;
+
+  return {
+    x: x - startX,
+    y: y - startY,
+    scaleX,
+    scaleY,
+  };
+}
+
+export function adminTransformed(
+  this: AdminHandler,
+  { x, y }: { x: number; y: number }
+): TransformedResult {
+  const { width, height } = this.global;
+
+  if (!width || !height) return { x, y, scaleX: 1, scaleY: 1 };
+
+  const doNotScale =
+    width <= MAX_IFRAME_SIZE.width && height <= MAX_IFRAME_SIZE.height;
+
+  if (doNotScale) return { x, y, scaleX: 1, scaleY: 1 };
+
+  let scaleX = 1;
+  let scaleY = 1;
+
+  if (width > MAX_IFRAME_SIZE.width) {
+    scaleX = this.p.width / width;
+  }
+
+  if (height > MAX_IFRAME_SIZE.height) {
+    scaleY = this.p.height / height;
+  }
+
+  return { x, y, scaleX, scaleY };
+}
