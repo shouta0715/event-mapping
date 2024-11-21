2f85b327045f8e4c598978c3db90001c1b0616d1
shouta0715 <Myheroes0715@gmail.com>
2024-11-21 22:14:38 +0900

    fix: get shape

--- packages/event-sdk/src/handlers/shape/get-shape/index.ts
+++ packages/event-sdk/src/handlers/shape/get-shape/index.ts
@@ -1,15 +1,15 @@
 import { Circle, Rectangle } from "@timohausmann/quadtree-ts";
 import { Shape } from "@event-mapping/event-sdk/handlers/shape";
 
-export function getShape<TData>(this: Shape): Circle<TData> | Rectangle<TData> {
-  let shape: Circle<TData> | Rectangle<TData>;
+export function getShape<TData>(this: Shape): Rectangle<TData> | Circle<TData> {
+  let shape: Rectangle<TData> | Circle<TData>;
 
-  if ("d" in this.size) {
+  if (this.options?.isCircle) {
     shape = new Circle<TData>({
       x: this.position.x,
       y: this.position.y,
-      r: this.size.d,
-      data: this.data,
+      r: this.size.w,
+      data: this.meta,
     });
   } else {
     shape = new Rectangle<TData>({
@@ -17,7 +17,6 @@ export function getShape<TData>(this: Shape): Circle<TData> | Rectangle<TData> {
       y: this.position.y,
       width: this.size.w,
       height: this.size.h,
-      data: this.data,
     });
   }
 
