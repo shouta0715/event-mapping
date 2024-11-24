ed91838fd2e1140f347f56abb4209b8af1397631
shouta0715 <Myheroes0715@gmail.com>
2024-11-24 17:45:00 +0900

    fix: query devtools

--- apps/admin/src/components/providers/index.tsx
+++ apps/admin/src/components/providers/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import { Toaster } from "sonner";
 
@@ -24,7 +23,6 @@ export function Providers({
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
       <Toaster
         closeButton
         position="top-right"
