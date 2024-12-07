fdc7f9ac9956f33d3cb393a44ff8e6ec5b2a3dbf
shouta0715 <Myheroes0715@gmail.com>
2024-12-07 20:28:16 +0900

    chore

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -86,15 +86,6 @@ async function main() {
     console.log(
       `  ${chalk.bold("2.")} ${chalk.cyan(`pnpm run dev:${folderName}`)}`
     );
-    console.log(
-      `  ${chalk.bold("3.")} ${chalk.cyan(`管理画面にアクセス後コンテンツを作成してください`)}`
-    );
-    console.log(
-      `  ${chalk.bold("3.")} ${chalk.cyan(`管理画面にアクセス後コンテンツを作成してください`)}`
-    );
-    console.log(
-      `apps/events/${folderName}/.env.localのVITE_SOURCE_IDに作成したコンテンツのIDを設定してください。`
-    );
     console.log(line);
   } catch (error) {
     console.error("❌ エラーが発生しました:", error);
