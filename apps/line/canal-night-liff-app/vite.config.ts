f3334430ced3c79cedb02a736d271b17f8a5d2ef
shouta0715 <Myheroes0715@gmail.com>
2024-12-13 21:30:04 +0900

    chore

--- apps/line/canal-night-liff-app/vite.config.ts
+++ apps/line/canal-night-liff-app/vite.config.ts
@@ -1,10 +1,9 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
