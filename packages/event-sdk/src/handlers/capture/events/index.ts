3d336776644ae5a43476352a5e46b71a258c1488
shouta0715 <Myheroes0715@gmail.com>
2024-12-15 17:32:53 +0900

    fix: event caputure

--- packages/event-sdk/src/handlers/capture/events/index.ts
+++ packages/event-sdk/src/handlers/capture/events/index.ts
@@ -1,11 +1,36 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import p5 from "p5";
+import { StreamingAnswerAction } from "@event-mapping/schema";
 import { EventHandler } from "@event-mapping/event-sdk/handlers/event";
+import { EventWebRTC } from "@event-mapping/event-sdk/handlers/rtc/events";
+
+function sendStreamingAnswer(
+  this: EventHandler,
+  data: StreamingAnswerAction["data"]
+) {
+  const message: StreamingAnswerAction = {
+    action: "streamingAnswer",
+    data,
+  };
+
+  this.ws.send(JSON.stringify(message));
+}
 
 export function createCapture(
   this: EventHandler,
-  type: "video" | "audio",
-  options?: { flipped: boolean }
-) {
-  return this.p.createCapture(type, options as any) as p5.MediaElement;
+  onTrack: (stream: MediaStream) => void
+): EventWebRTC | null {
+  if (!this.terminal?.id) {
+    // eslint-disable-next-line no-console
+    console.warn("No terminal id found");
+
+    return null;
+  }
+
+  const rtc = new EventWebRTC({
+    id: this.terminal?.id,
+    streamingAnswer: sendStreamingAnswer.bind(this),
+    onTrack,
+  });
+
+  return rtc;
 }
