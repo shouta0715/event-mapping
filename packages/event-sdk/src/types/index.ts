a3b4c2d74062fa1753e33d7a1ed453c4b3110948
shouta0715 <Myheroes0715@gmail.com>
2024-12-17 02:26:28 +0900

    chore

--- packages/event-sdk/src/types/index.ts
+++ packages/event-sdk/src/types/index.ts
@@ -1,3 +1,4 @@
+import { ProcessorAction } from "@event-mapping/schema";
 import p5 from "p5";
 
 export * from "./admin";
@@ -59,5 +60,5 @@ export interface EventClient {
     options?: { flipped: boolean }
   ) => p5.MediaElement | null;
   capture: p5.MediaElement | null;
-  processor<T extends Record<string, unknown>>(generator: () => T): T | null;
+  processor<T extends ProcessorAction["data"]>(generator: () => T): T | null;
 }
